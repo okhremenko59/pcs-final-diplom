96d0554eb2764df170be876913f5db78eb40f2f2
okhremenko59 <okhremenko@bk.ru>
2023-01-25 13:27:09 +0500

    work

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,56 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    protected static Map<String, List<PageEntry>> words;
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        List<File> listOfPDFFiles = List.of(Objects.requireNonNull(pdfsDir.listFiles()));
+
+        words = new HashMap<>();
+
+        for (File pdf : listOfPDFFiles) {
+
+            var doc = new PdfDocument(new PdfReader(pdf));
+
+            for (int i = 0; i < doc.getNumberOfPages(); i++) {
+
+                var textOfOnePage = PdfTextExtractor.getTextFromPage(doc.getPage(i + 1));
+                var allWordsOnPage = textOfOnePage.split("\\P{IsAlphabetic}+");
+                Map<String, Integer> freqs = new HashMap<>();
+
+
+                for (var word : allWordsOnPage) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    freqs.put(word.toLowerCase(), freqs.getOrDefault(word.toLowerCase(), 0) + 1);
+                }
+
+                int count = 0;
+                for (var word : freqs.keySet()) {
+                    String wordToLowerCase = word.toLowerCase();
+                    if (freqs.get(wordToLowerCase) != null) {
+                        count = freqs.get(wordToLowerCase);
+                        words.computeIfAbsent(wordToLowerCase, k -> new ArrayList<>()).add(new PageEntry(pdf.getName(), i + 1, count));
+                    }
+                }
+                freqs.clear();
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        String wordToLowReg = word.toLowerCase();
+        List<PageEntry> pageEntries = words.getOrDefault(wordToLowReg, Collections.emptyList());
+
+        Collections.sort(pageEntries);
+        return pageEntries;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,50 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+    static int port = 8989;
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+    public static void main(String[] args) {
+
+        try {
+            BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+            List<PageEntry> resultList = engine.search("Бизнес");
+
+            for (PageEntry pageEntry : resultList) {
+                System.out.println(pageEntry.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+            Gson gson = new GsonBuilder().create();
+
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                    String word = in.readLine();
+                    List<PageEntry> page = engine.search(word);
+
+                    if (page == null) {
+                        out.println("Совпадения отсутствуют");
+                    }
+
+                    var json = gson.toJson(page);
+                    out.println(json);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
